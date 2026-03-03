aaba5eec9c9c8304d5597bf2de06cbedeafad1d6
NELLURI_KOUSHIK <160224734+nkoushik30@users.noreply.github.com>
2026-03-03 21:45:26 +0530

    Update and rename javaTest.java to AppTest.java

--- AppTest.java
+++ AppTest.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class javaTest
+public class AppTest
 {
 
     public static void main(String[] args)
